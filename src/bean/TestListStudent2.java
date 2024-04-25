b5f5334e749d9c16d38d5166b576760306513334
RIKUTO0625 <gojira1974612@gmail.com>
2024-04-25 12:39:45 +0900

    ゲッターセッターなど書き込み

--- src/bean/TestListStudent2.java
+++ src/bean/TestListStudent2.java
@@ -2,4 +2,12 @@ package bean;
 
 public class TestListStudent2 {
 
+
+	private String subjectName;
+
+	private String subjectCD;
+
+	private int num;
+
+	private int point;
 }
--- src/bean/TestListSubject2.java
+++ src/bean/TestListSubject2.java
@@ -1,5 +1,72 @@
 package bean;
 
+import java.util.Map;
+
 public class TestListSubject2 {
+
+
+	private int entYear;
+
+	private String studentNo;
+
+	private String studentName;
+
+	private String classNum;
+
+	private Map<Integer,Integer> points;
+
+
+	//getter setter
+
+	public int getEntYear() {
+		return entYear;
+	}
+
+	public void setEntYear(int entYear) {
+		this.entYear = entYear;
+	}
+
+	public String getStudentNo() {
+		return studentNo;
+	}
+
+	public void setStudentNo(String studentNo) {
+		this.studentNo = studentNo;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getClassNum() {
+		return classNum;
+	}
+
+	public void setClassNum(String classNum) {
+		this.classNum = classNum;
+	}
+
+	public Map<Integer, Integer> getPoints() {
+		return points;
+	}
+
+	public void setPoints(Map<Integer, Integer> points) {
+		this.points = points;
+	}
+
+
+	//点数をマップリストから取得？
+	public String getPoint(int key){
+
+	}
+
+	//点数をマップリストから取得？
+	public void putPoint(int key,int value){
+
+	}
 
 }
