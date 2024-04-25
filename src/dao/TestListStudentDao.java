d840265e0d6ccf36cd86b09c2502fe41c8fbafa3
Kazu121430 <knz2374439@stu.o-hara.ac.jp>
2024-04-25 15:04:47 +0900

    空→ちょっと付け加え

--- src/dao/TestListStudentDao.java
+++ src/dao/TestListStudentDao.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import bean.Student;
@@ -8,7 +9,24 @@ import bean.TestListStudent;
 
 public class TestListStudentDao extends Dao{
 
+	/**
+	 * baseSql:String 共通SQL文 プライベート
+	 */
+	private String baseSql = "select * from student where school_cd=? ";
+
+
+	/**
+	 * postFilterメソッド フィルター後のリストへの格納処理 プライベート
+	 *
+	 * @param rSet:リザルトセット
+	 * @return 学生のリスト:List<TestListStudent> 存在しない場合は0件のリスト
+	 * @throws Exception
+	 */
 	private List<TestListStudent> postFilter(ResultSet rSet) throws Exception {
+
+		// リストを初期化
+		List<TestListStudent> list = new ArrayList<>();
+
 
 	}
 
--- src/scoremanager/main/TestListAction.java
+++ src/scoremanager/main/TestListAction.java
@@ -1,5 +1,34 @@
 package scoremanager.main;
 
-public class TestListAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Teacher;
+import tool.Action;
+
+public class TestListAction extends Action{
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+		//ローカル変数の宣言 1
+		HttpSession session = req.getSession();//セッション
+		Teacher teacher = (Teacher) session.getAttribute("user");// ログインユーザーを取得
+
+
+		//リクエストパラメータ―の取得 2
+
+		//DBからデータ取得 3
+
+		//ビジネスロジック 4
+
+		//DBへデータ保存 5
+		//なし
+
+		//レスポンス値をセット 6
+
+
+	}
 
 }
