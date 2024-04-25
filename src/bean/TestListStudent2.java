a8067a9a2d1a84888f616619368b7f970747b855
RIKUTO0625 <gojira1974612@gmail.com>
2024-04-25 12:39:58 +0900

    setterなど

--- src/bean/TestListStudent2.java
+++ src/bean/TestListStudent2.java
@@ -10,4 +10,43 @@ public class TestListStudent2 {
 	private int num;
 
 	private int point;
+	
+	
+	//getter setter
+
+	public String getSubjectName() {
+		return subjectName;
+	}
+
+	public void setSubjectName(String subjectName) {
+		this.subjectName = subjectName;
+	}
+
+	public String getSubjectCD() {
+		return subjectCD;
+	}
+
+	public void setSubjectCD(String subjectCD) {
+		this.subjectCD = subjectCD;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	
+
+
+	
 }
