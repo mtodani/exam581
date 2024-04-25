4db9ea94e4a9cb998984b23f9b17ab1f96c4db68
RIKUTO0625 <gojira1974612@gmail.com>
2024-04-25 15:41:51 +0900

    TestListSubjectDao

--- src/dao/TestListSubjectDao2.java
+++ src/dao/TestListSubjectDao2.java
@@ -1,14 +1,21 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import bean.School;
+import bean.Student;
+import bean.Subject;
 import bean.TestListSubject;
 
-public class TestListSubjectDao2 {
+public class TestListSubjectDao2 extends Dao{
 
+	
+	
 	private List<TestListSubject> postFilter(ResultSet rSet) throws Exception {
 		//まずはここに処理追加
 		 // リストを初期化
@@ -33,6 +40,60 @@ public class TestListSubjectDao2 {
 	    }
 
 	    return list;
+	}
+
+
+	//フィルター（検索の時に使いそう？）
+	public List<Student> filter(School school, int entYear, String classNum, Subject subject) throws Exception {
+		//まずはここに処理追加
+		List<Student> list = new ArrayList<>();
+        Connection connection = getConnection();
+        PreparedStatement statement = null;
+        ResultSet rSet = null;
+
+        String condition = "and ent_year=? and class_num=?";
+        String order = " order by student_no asc";
+
+        String conditionIsAttend = "";
+
+        try {
+            statement = connection.prepareStatement("select * from test" + condition + conditionIsAttend + order);
+            statement.setString(1, school.getSchool_cd());
+            statement.setInt(2, entYear);
+            statement.setString(3, classNum);
+
+            rSet = statement.executeQuery();
+            list = postFilter(rSet, school);
+        }catch(Exception e){
+			throw e;
+
+		}finally {
+            // Close resources in finally block to ensure they're always closed
+
+            if (statement != null) {
+            	try {
+
+            		statement.close();
+
+				} catch (SQLException sqle) {
+					// TODO: handle exception
+					throw sqle;
+				}
+
+            }
+            if (connection != null) {
+            	try {
+            		 connection.close();
+
+				} catch (SQLException sqle) {
+					// TODO: handle exception
+					throw sqle;
+				}
+
+            }
+        }
+
+        return list;
 	}
 
 }
