b0b75a133b084ea059911c1795223c9bbfe83bb5
hunwari <knz2374552@stu.o-hara.an.jp>
2024-04-25 15:16:24 +0900

    subjectdao

--- src/dao/SubjectDao.java
+++ src/dao/SubjectDao.java
@@ -7,24 +7,22 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.security.auth.Subject;
-
 import bean.School;
 import bean.Subject;
 
 public class SubjectDao extends Dao{
 
 	/**
-	 * getメソッド 学生番号を指定して学生インスタンスを1件取得する
+	 * getメソッド 科目コードと学校コードを指定して科目インスタンスを1件取得する
 	 *
 	 * @param cd:String
 	 *            科目コード
-	 * @return 学生クラスのインスタンス 存在しない場合はnull
+	 * @return 科目クラスのインスタンス 存在しない場合はnull
 	 * @throws Exception
 	 */
 
 
-	public Subject get(String cd) throws Exception {
+	public Subject get(String subject_cd,School school) throws Exception {
 		Subject subject = new Subject();
 
 	    Connection connection = getConnection();
@@ -33,28 +31,24 @@ public class SubjectDao extends Dao{
 
 	    try {
 
-
 	        // SQL文を作成
-	        String sql = "SELECT * FROM student WHERE subject_cd = ?";
+	        String sql = "SELECT * FROM subject WHERE subject_cd = ?subject_cd = ? AND school_cd = ?";
 	        statement = connection.prepareStatement(sql);
-	        statement.setString(1, cd);
+	        statement.setString(1, subject_cd);
+	        statement.setString(2, school.getSchool_cd());
+
 
 		    ResultSet rSet = statement.executeQuery();
 
 		    SchoolDao schoolDao = new SchoolDao();
 
 
-
-
 	        // リザルトセットから学生インスタンスを作成
 	        if (rSet.next()) {
 	            subject.setSubject_cd(rSet.getString("subject_cd"));
 	            subject.setSubject_name(rSet.getString("subject_name"));
+	            subject.setSchool(school);
 
-	            // 学校インスタンスを取得して設定
-	            School school = new School();
-	            ;
-	            subject.setSchool(schoolDao.get(rSet.getString("school_cd")));
 	        }
 	        else{
 	        	subject= null;
@@ -88,80 +82,36 @@ public class SubjectDao extends Dao{
 	/**
 	 * baseSql:String 共通SQL文 プライベート
 	 */
-	private String baseSql = "select * from student where school_cd=? ";
+	private String baseSql = "select * from subject where school_cd=? ";
+
 
 	/**
-	 * postFilterメソッド フィルター後のリストへの格納処理 プライベート
-	 *
-	 * @param rSet:リザルトセット
-	 * @param school:School
-	 *            学校
-	 * @return 学生のリスト:List<Student> 存在しない場合は0件のリスト
-	 * @throws Exception
-	 */
-	private List<Subject> postFilter(ResultSet rSet, School school) throws Exception {
-		//まずはここに処理追加
-		 // リストを初期化
-	    List<Subject> list = new ArrayList<>();
-
-	    try {
-	        // リザルトセットを全権走査
-	        while (rSet.next()) {
-	            // 学生インスタンスを初期化
-	        	Subject subject = new Subject();
-
-	            // 学生インスタンスに検索結果をセット
-	            subject.setSubject_cd(rSet.getString("subject_co"));
-	            subject.setSubject_name(rSet.getString("subject_name"));
-	            subject.setSchool(school);
-
-	            // リストに追加
-	            list.add(subject);
-	        }
-	    } catch (SQLException | NullPointerException e) {
-	        e.printStackTrace();
-	    }
-
-	    return list;
-	}
-
-	/**
-	 * filterメソッド 学校、入学年度、クラス番号、在学フラグを指定して学生の一覧を取得する
+	 * filterメソッド 学校を指定して科目の一覧を取得する
 	 *
 	 * @param school:School
 	 *            学校
-	 * @param entYear:int
-	 *            入学年度
-	 * @param classNum:String
-	 *            クラス番号
-	 * @param isAttend:boolean
-	 *            在学フラグ
-	 * @return 学生のリスト:List<Student> 存在しない場合は0件のリスト
+
+	 * @return 科目のリスト:List<Student> 存在しない場合は0件のリスト
 	 * @throws Exception
 	 */
-	public List<Subject> filter(School school, int entYear, String classNum, boolean isAttend) throws Exception {
+	public List<Subject> filter(School school) throws Exception {
 		//まずはここに処理追加
-		List<Student> list = new ArrayList<>();
+		List<Subject> list = new ArrayList<>();
         Connection connection = getConnection();
         PreparedStatement statement = null;
         ResultSet rSet = null;
 
-        String condition = "and ent_year=? and class_num=?";
-        String order = " order by student_no asc";
+
+        String order = " order by subject_cd asc";
 
         String conditionIsAttend = "";
-        if (isAttend) {
-            conditionIsAttend = "and is_attend=true";
-        }
+
 
         try {
-            statement = connection.prepareStatement(baseSql + condition + conditionIsAttend + order);
+            statement = connection.prepareStatement(baseSql + conditionIsAttend + order);
             statement.setString(1, school.getSchool_cd());
-            statement.setInt(2, entYear);
-            statement.setString(3, classNum);
 
             rSet = statement.executeQuery();
-            list = postFilter(rSet, school);
         }catch(Exception e){
 			throw e;
 
@@ -194,197 +144,6 @@ public class SubjectDao extends Dao{
         return list;
 	}
 
-	/**
-	 * filterメソッド 学校、入学年度、在学フラグを指定して学生の一覧を取得する
-	 *
-	 * @param school:School
-	 *            学校
-	 * @param entYear:int
-	 *            入学年度
-	 * @param isAttend:boolean
-	 *            在学フラグ
-	 * @return 学生のリスト:List<Student> 存在しない場合は0件のリスト
-	 * @throws Exception
-	 */
-	public List<Subject> filter(School school, int entYear, boolean isAttend) throws Exception {
-		//まずはここに処理追加
-		List<Student> list = new ArrayList<>();
-        Connection connection = getConnection();
-        PreparedStatement statement = null;
-        ResultSet rSet = null;
-        String condition = "and ent_year=?";
-        String order = " order by student_no asc";
-        // 在学フラグ
-        String conditionIsAttend = "";
-        if (isAttend) {
-            conditionIsAttend = "and is_attend=true";
-        }
-        try {
-            // プリペアドステートメントを実行
-            statement = connection.prepareStatement(baseSql + condition + conditionIsAttend + order);
-            statement.setString(1, school.getSchool_cd());
-            statement.setInt(2, entYear);
-            rSet = statement.executeQuery();
-            list = postFilter(rSet, school);
-
-        } catch (Exception e) {
-            throw e;
-        }finally {
-            // Close resources in finally block to ensure they're always closed
-
-            if (statement != null) {
-            	try {
-
-            		statement.close();
-
-				} catch (SQLException sqle) {
-					// TODO: handle exception
-					throw sqle;
-				}
-
-            }
-            if (connection != null) {
-            	try {
-            		 connection.close();
-
-				} catch (SQLException sqle) {
-					// TODO: handle exception
-					throw sqle;
-				}
-
-            }
-        }
-
-        return list;
-	}
-
-	/**
-	 * filterメソッド 学校、在学フラグを指定して学生の一覧を取得する
-	 *
-	 * @param school:School
-	 *            学校
-	 * @param isAttend:boolean
-	 *            在学フラグ
-	 * @return 学生のリスト:List<Student> 存在しない場合は0件のリスト
-	 * @throws Exception
-	 */
-	public List<Subject> filter(School school, boolean isAttend) throws Exception {
-		//まずはここに処理追加
-		List<Student> list = new ArrayList<>();
-        Connection connection = getConnection();
-        PreparedStatement statement = null;
-        ResultSet rSet = null;
-        String order = " order by student_no asc";
-
-        // SQL文の作成
-        String conditionIsAttend = "";
-        if (isAttend) {
-            conditionIsAttend = "and is_attend=true";
-        }
-
-        try {
-            statement = connection.prepareStatement(baseSql + conditionIsAttend + order);
-            statement.setString(1, school.getSchool_cd());
-            rSet = statement.executeQuery();
-
-            list = postFilter(rSet, school);
-        } catch (Exception e) {
-            throw e;
-        }finally {
-            // Close resources in finally block to ensure they're always closed
-
-            if (statement != null) {
-            	try {
-
-            		statement.close();
-
-				} catch (SQLException sqle) {
-					// TODO: handle exception
-					throw sqle;
-				}
-
-            }
-            if (connection != null) {
-            	try {
-            		 connection.close();
-
-				} catch (SQLException sqle) {
-					// TODO: handle exception
-					throw sqle;
-				}
-
-            }
-        }
-
-        return list;
-	}
-
-
-
-	/**
-	 * saveメソッド 学生インスタンスをデータベースに保存する データが存在する場合は更新、存在しない場合は登録
-	 *
-	 * @param student：Student
-	 *            学生
-	 * @return 成功:true, 失敗:false
-	 * @throws Exception
-	 */
-//	public boolean save(Student student) throws Exception {
-//		//次はここに処理追加
-//	}
-
-//	public boolean save(Student student) throws Exception {
-//	    boolean result = false;
-//	    Connection connection = null;
-//	    PreparedStatement statement = null;
-//
-//	    try {
-//	        // データベース接続を取得
-//	        connection = getConnection();
-//
-//	        // SQLを作成
-//	        String sql = "INSERT INTO student (student_no, student_name, ent_year, class_num, is_attend, school_cd) " +
-//	                     "VALUES (?, ?, ?, ?, ?, ?) " +
-//	                     "ON DUPLICATE KEY UPDATE student_name = ?, ent_year = ?, class_num = ?, is_attend = ?";
-//
-//	        statement = connection.prepareStatement(sql);
-//	        statement.setString(1, student.getStudent_no());
-//	        statement.setString(2, student.getStudent_name());
-//	        statement.setInt(3, student.getEntYear());
-//	        statement.setString(4, student.getClass_num());
-//	        statement.setBoolean(5, student.isAttend());
-//	        statement.setString(6, student.getSchool().getSchool_cd());
-//	        statement.setString(7, student.getStudent_name());
-//	        statement.setInt(8, student.getEntYear());
-//	        statement.setString(9, student.getClass_num());
-//	        statement.setBoolean(10, student.isAttend());
-//
-//	        // SQLを実行
-//	        int rows = statement.executeUpdate();
-//	        result = rows > 0;
-//	    } catch (SQLException e) {
-//	        e.printStackTrace();
-//	        throw e;
-//	    } finally {
-//	        // リソースを閉じる
-//	        if (statement != null) {
-//	            try {
-//	                statement.close();
-//	            } catch (SQLException e) {
-//	                e.printStackTrace();
-//	            }
-//	        }
-//	        if (connection != null) {
-//	            try {
-//	                connection.close();
-//	            } catch (SQLException e) {
-//	                e.printStackTrace();
-//	            }
-//	        }
-//	    }
-//
-//	    return result;
-//	}
 
 	public boolean save (Subject subject) throws Exception {
 		//コネクションを確立
@@ -396,12 +155,12 @@ public class SubjectDao extends Dao{
 
 		try {
 			// データベースから科目を取得
-			Student old = get (subject.getSubject_cd());
+			Subject old = get (subject.getSubject_cd(),subject.getSchool());
 			if(old == null) {
 				// 科目が存在しなかった場合
 				// プリペアードステートメンにINSERT文をセットと
 				statement = connection. prepareStatement (
-				"insert into student (subject_co,subject_name,school_cd) values(?, ?, ? )");
+				"insert into student (subject_cd,subject_name,school_cd) values(?, ?, ? )");
 				// プリペアードステートメントに値をバインド
 				statement.setString(1,subject.getSubject_cd()) ;
 				statement.setString (2,subject.getSubject_name()) ;
--- src/scoremanager/main/SubjectListAction.java
+++ src/scoremanager/main/SubjectListAction.java
@@ -1,7 +1,6 @@
 package scoremanager.main;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import bean.Student;
+import bean.Subject;
 import bean.Teacher;
 import dao.ClassNumDao;
 import dao.StudentDao;
@@ -30,98 +29,23 @@ public class SubjectListAction extends Action {
 		String isAttendStr="";//入力された在学フラグ
 		int entYear = 0;// 入学年度
 		boolean isAttend = false;// 在学フラグ
-		List<Student> students = null;// 学生リスト
+		List<Subject> students = null;// 学生リスト
 		LocalDate todaysDate = LocalDate.now();// LcalDateインスタンスを取得
 		int year = todaysDate.getYear();// 現在の年を取得
 		StudentDao sDao = new StudentDao();//学生Dao
 		ClassNumDao cNumDao = new ClassNumDao();// クラス番号Daoを初期化
 		Map<String, String> errors = new HashMap<>();// エラーメッセージ
 
-		//リクエストパラメータ―の取得 2
-		entYearStr = req.getParameter("f1");
-		classNum = req.getParameter("f2");
-		isAttendStr = req.getParameter("f3");
-
-		if (isAttendStr != null) {
-			// 在学フラグを立てる
-			isAttend = true;
-		}
-
-		//DBからデータ取得 3
-		// ログインユーザーの学校コードをもとにクラス番号の一覧を取得
-		List<String> list = cNumDao.filter(teacher.getSchool());
-
-		//ビジネスロジック 4
-		if (entYearStr != null) {
-			// 数値に変換
-			entYear = Integer.parseInt(entYearStr);
-		}
 
 
 
-			//ここに昨日作成した部分をコピペしてみよう！！
-		if (entYear != 0 && !classNum.equals("0")) {
-			// 入学年度とクラス番号を指定
-			students = sDao.filter(teacher.getSchool(), entYear, classNum, isAttend);
-			System.out.println("1");
-		} else if (entYear != 0 && classNum.equals("0")) {
-			// 入学年度のみ指定
-			students = sDao.filter(teacher.getSchool(), entYear, isAttend);
-			System.out.println("2");
-
-		} else if (entYear == 0 && classNum == null || entYear == 0 && classNum.equals("0")) {
-			// 指定なしの場合
-			// 全学生情報を取得
-			students = sDao.filter(teacher.getSchool(), isAttend);
-			System.out.println("3");
-
-		} else {
-			errors.put("f1", "クラスを指定する場合は入学年度も指定してください");
-			req.setAttribute("errors", errors);
-			// 全学生情報を取得
-			students = sDao.filter(teacher.getSchool(), isAttend);
-			System.out.println("4");
-
-		}
-
-
-//		//ビジネスロジック 4
-//		if (entYearStr != null) {
-//			// 数値に変換
-//			entYear = Integer.parseInt(entYearStr);
-//		}
-
-
-
-
-		// リストを初期化
-		List<Integer> entYearSet = new ArrayList<>();
-		// 10年前から1年後まで年をリストに追加
-		for (int i = year - 10; i < year + 1; i++) {
-			entYearSet.add(i);
-		}
-
-		//DBへデータ保存 5
-		//なし
-		//レスポンス値をセット 6
-		// リクエストに入学年度をセット
-		req.setAttribute("f1", entYear);
-		// リクエストにクラス番号をセット
-		req.setAttribute("f2", classNum);
-		// 在学フラグが送信されていた場合
-		if (isAttendStr != null) {
-			// リクエストに在学フラグをセット
-			req.setAttribute("f3", isAttendStr);
-		}
 		// リクエストに学生リストをセット
 		req.setAttribute("students", students);
-		// リクエストにデータをセット
-		req.setAttribute("class_num_set", list);
-		req.setAttribute("ent_year_set", entYearSet);
+
 		//JSPへフォワード 7
 		req.getRequestDispatcher("student_list.jsp").forward(req, res);
 	}
 
-}{
-
 }
+
+
