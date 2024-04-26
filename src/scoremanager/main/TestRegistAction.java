ed7c1dcf57f571d34d679600761d7dd56e4c2dd6
Tkm2004 <knz2374528@stu.o-hara.ac.jp>
2024-04-26 12:42:51 +0900

    それぞれ内容更新

--- src/scoremanager/main/TestRegistAction.java
+++ src/scoremanager/main/TestRegistAction.java
@@ -13,10 +13,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import bean.Student;
 import bean.Teacher;
+import bean.Test;
 import dao.ClassNumDao;
-import dao.StudentDao;
+import dao.TestDao;
 import tool.Action;
 
 public class TestRegistAction extends Action {
@@ -30,20 +30,27 @@ public class TestRegistAction extends Action {
 
 		String entYearStr="";// 入力された入学年度
 		String classNum = "";//入力されたクラス番号
-		String isAttendStr="";//入力された在学フラグ
+		String isAttendStr="";//入力された在学フラグ 後から使うかも
+		String subject="";//入力された科目
+		String numStr = "";// 入力された回数
+		int num = 0;// 回数
 		int entYear = 0;// 入学年度
-		boolean isAttend = false;// 在学フラグ
-		List<Student> students = null;// 学生リスト
+		boolean isAttend = false;// 在学フラグ 後から使うかも
+		List<Test> tests = null;// 学生リスト
 		LocalDate todaysDate = LocalDate.now();// LcalDateインスタンスを取得
 		int year = todaysDate.getYear();// 現在の年を取得
-		StudentDao sDao = new StudentDao();//学生Dao
+		TestDao tDao = new TestDao();//学生Dao
 		ClassNumDao cNumDao = new ClassNumDao();// クラス番号Daoを初期化
 		Map<String, String> errors = new HashMap<>();// エラーメッセージ
 
 		//リクエストパラメータ―の取得 2
+
+
+		//リクエストパラメータ―の取得 2
 		entYearStr = req.getParameter("f1");
 		classNum = req.getParameter("f2");
-		isAttendStr = req.getParameter("f3");
+		subject = req.getParameter("f3");
+		numStr = req.getParameter("f4");
 
 		if (isAttendStr != null) {
 			// 在学フラグを立てる
@@ -60,29 +67,26 @@ public class TestRegistAction extends Action {
 			entYear = Integer.parseInt(entYearStr);
 		}
 
-		if (entYear != 0 && !classNum.equals("0")) {
-			// 入学年度とクラス番号を指定
-			students = sDao.filter(teacher.getSchool(), entYear, classNum, isAttend);
-			System.out.println("1");
-		} else if (entYear != 0 && classNum.equals("0")) {
-			// 入学年度のみ指定
-			students = sDao.filter(teacher.getSchool(), entYear, isAttend);
-			System.out.println("2");
+		if (numStr != null) {
+			// 数値に変換
+			num = Integer.parseInt(numStr);
+		}
 
-		} else if (entYear == 0 && classNum == null || entYear == 0 && classNum.equals("0")) {
-			// 指定なしの場合
-			// 全学生情報を取得
-			students = sDao.filter(teacher.getSchool(), isAttend);
-			System.out.println("3");
+		if (entYear != 0 && !classNum.equals("0")&&subject.equals("0")&& num != 0){
+			// 入学年度、クラス番号、科目、回数
 
-		} else {
-			errors.put("f1", "クラスを指定する場合は入学年度も指定してください");
-			req.setAttribute("errors", errors);
-			// 全学生情報を取得
-			students = sDao.filter(teacher.getSchool(), isAttend);
-			System.out.println("4");
+			//studentsリストに
+			tests = tDao.filter(teacher.getSchool(), entYear, classNum, subject, num, isAttend);
 
 		}
+//		else {
+//			errors.put("f1", "クラスを指定する場合は入学年度も指定してください");
+//			req.setAttribute("errors", errors);
+//			// 全学生情報を取得
+//			tests = tDao.filter(teacher.getSchool(), isAttend);
+//			System.out.println("4");
+//
+//		}
 
 		//ビジネスロジック 4
 		// リストを初期化
@@ -100,15 +104,20 @@ public class TestRegistAction extends Action {
 		// リクエストにクラス番号をセット
 		req.setAttribute("f2", classNum);
 		// 在学フラグが送信されていた場合
-		if (isAttendStr != null) {
-			// リクエストに在学フラグをセット
-			req.setAttribute("f3", isAttendStr);
-		}
+//		if (isAttendStr != null) {
+//			// リクエストに在学フラグをセット
+//			req.setAttribute("f3", isAttendStr);
+//		}
+		req.setAttribute("f3", subject);
+		req.setAttribute("f4", num);
 		// リクエストに学生リストをセット
-		req.setAttribute("students", students);
+		req.setAttribute("tests", tests);
 		// リクエストにデータをセット
 		req.setAttribute("class_num_set", list);
 		req.setAttribute("ent_year_set", entYearSet);
+		req.setAttribute("subject ?");//今は形だけ
+		req.setAttribute("numStr ?");//形だけ
+		//↑のこいつらをstudent_list.jspに渡す
 		//JSPへフォワード 7
 		req.getRequestDispatcher("student_list.jsp").forward(req, res);
 	}
