b92dbc1d573bab93f3dc931ab6eb0cde37c06d42
Kazu121430 <knz2374439@stu.o-hara.ac.jp>
2024-05-01 13:34:21 +0900

    TLSubExeの更新

--- src/scoremanager/main/TestListSubjectExecuteAction.java
+++ src/scoremanager/main/TestListSubjectExecuteAction.java
@@ -9,10 +9,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bean.School;
+import bean.Subject;
 import bean.Teacher;
+import bean.TestListSubject;
 import dao.ClassNumDao;
 import dao.SubjectDao;
-import dao.TestListSubjectDao2;
+import dao.TestListSubjectDao;
 import tool.Action;
 
 public class TestListSubjectExecuteAction extends Action{
@@ -23,12 +26,14 @@ public class TestListSubjectExecuteAction extends Action{
 		//ローカル変数の宣言 1
 		HttpSession session = req.getSession();   // セッション
 		Teacher teacher = (Teacher) session.getAttribute("user");   // ログインユーザーを取得
-		TestListSubjectDao2 TLSubDao2 = new TestListSubjectDao2();   // 成績Daoを初期化
+		TestListSubjectDao TLSubDao = new TestListSubjectDao();   // 成績Daoを初期化
 		SubjectDao SubDao = new SubjectDao();  // 科目Daoを初期化
 
 		int entYear;   // 入学年度
 		String classNum = "";   // クラス番号
 		String subjectCd = "";   // 科目
+		Map<Integer,Integer> points = new HashMap<>();   // 成績
+		List<TestListSubject> TLSubList = new ArrayList<>();   //空の成績リスト
 
 		List<Integer> entYearSet = new ArrayList<>();   // 入学年度のリストを初期化
 		ClassNumDao cNumDao = new ClassNumDao();   // クラス番号Daoを初期化
@@ -43,11 +48,18 @@ public class TestListSubjectExecuteAction extends Action{
 		req.setAttribute("f2", classNum);
 		req.setAttribute("f3", subjectCd);
 
-		//DBからデータ取得 3
-		//なし
 
 		//ビジネスロジック 4
-		//なし
+		// 入学年度が選択されていない場合
+		if (entYear == 0 || classNum == "0" || subjectCd == "0") {
+			errors.put("select", "入学年度、クラス、科目のすべてを選択してください");
+		} else {
+
+			//DBからデータ取得 3
+			School school = teacher.getSchool();
+			Subject sub = SubDao.get(subjectCd,school);
+			TLSubList = TLSubDao.filter(school, entYear, classNum, sub);
+		}
 
 		//DBへデータ保存 5
 		//なし
