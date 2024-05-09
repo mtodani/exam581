2556bdd77f20a87b626b87c3b83535bdaad13fc0
mtodani <one4all4one.wwjd@gmail.com>
2024-05-09 12:21:39 +0900

    システムエラー（ＤＢ接続エラー等）に対応[DB接続エラー等のときにerror.jspに遷移]

--- src/dao/TeacherDao.java
+++ src/dao/TeacherDao.java
@@ -3,7 +3,6 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import bean.School;
 import bean.Teacher;
@@ -11,50 +10,48 @@ import bean.Teacher;
 public class TeacherDao extends Dao {
 
     // ログイン処理
-    public Teacher login(String id, String password) {
+    public Teacher login(String id, String password) throws Exception{
 
     	Teacher teacher = null;
 
     	//school_cdによってJOIN SCHOOLさせて、SCHOOL_nameがゲットできるように
         String sql = "SELECT * FROM TEACHER JOIN SCHOOL ON TEACHER.SCHOOL_CD = SCHOOL.SCHOOL_CD where teacher_id = ? and password = ?";
 
-        try (Connection conn = getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setString(1, id);
-            stmt.setString(2, password);
+        Connection conn = getConnection();
+	    PreparedStatement stmt = conn.prepareStatement(sql);
+	    stmt.setString(1, id);
+	    stmt.setString(2, password);
 
-            try (ResultSet rs = stmt.executeQuery()) {
-                if (rs.next()) { // 認証成功
+        ResultSet rs = stmt.executeQuery();
 
-                    String teacherName = rs.getString("TEACHER_NAME");
-                    String schoolCd = rs.getString("SCHOOL_CD");
-                    String schoolName = rs.getString("SCHOOL_NAME");
+        if (rs.next()) { // 認証成功の場合
 
-                    // Teacherオブジェクトを作成して設定
-                    teacher = new Teacher();
-                    teacher.setTeacher_id(id);
-                    teacher.setPassword(password);
-                    teacher.setTeacher_name(teacherName);
+            String teacherName = rs.getString("TEACHER_NAME");
+            String schoolCd = rs.getString("SCHOOL_CD");
+            String schoolName = rs.getString("SCHOOL_NAME");
+
+            // Teacherオブジェクトを作成して設定
+            teacher = new Teacher();
+            teacher.setTeacher_id(id);
+            teacher.setPassword(password);
+            teacher.setTeacher_name(teacherName);
 
 
-                    // Schoolオブジェクトを作成して設定
-                    School school = new School();
-                    school.setSchool_cd(schoolCd);
-                    school.setSchool_name(schoolName);
+            // Schoolオブジェクトを作成して設定
+            School school = new School();
+            school.setSchool_cd(schoolCd);
+            school.setSchool_name(schoolName);
 
-                    teacher.setSchool(school);
+            teacher.setSchool(school);
 
-                    // 認証済みフラグを設定
-                    teacher.setAuthenticated(true);
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace(); // 例外処理はまだ
-        } catch (Exception e) {
-        	e.printStackTrace();
+            // 認証済みフラグを設定
+            teacher.setAuthenticated(true);
         }
+
 
         //ログインされたteacherのデータを返す
         return teacher;
+
     }
+
 }
